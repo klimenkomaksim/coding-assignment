ea926fa7c622e2f9e899d1e841c3a6aa79c94ab9
Maksim_Klimenko <Maksim_Klimenko1@epam.com>
2023-08-02 14:51:32 +0100

    implement infinite scrolling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from 'react-redux'
 import 'reactjs-popup/dist/index.css'
-import { fetchMovies } from './data/moviesSlice'
+import { fetchMovies, fetchMoreMovies } from './data/moviesSlice'
 import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
 import Header from './components/Header'
 import Movies from './components/Movies'
@@ -74,6 +74,18 @@ const App = () => {
   useEffect(() => {
     getMovies()
   }, [])
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+        dispatch(fetchMoreMovies(ENDPOINT_DISCOVER));
+      }
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => window.removeEventListener("scroll", scrollHandler);
+  });
 
   return (
     <div className="App">
